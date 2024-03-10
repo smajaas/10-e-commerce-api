451c76d781553a78d2d6354a736f1eaedaa0441a
smajaas <smajaas@gmail.com>
2024-03-10 23:24:30 +0530

    basic routes

--- final/app.js
+++ final/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 
 //express
 const express = require('express');
@@ -6,6 +7,19 @@ const app = express();
 
 //database
 const connectDB = require('./db/connect');
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('e-commerce API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
