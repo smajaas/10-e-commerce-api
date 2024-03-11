74c596fe13ba5005827844ed78eefaaeeb73a47e
smajaas <smajaas@gmail.com>
2024-03-11 11:09:53 +0530

    unique email in schema and controller

--- final/controllers/authController.js
+++ final/controllers/authController.js
@@ -3,6 +3,14 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 const register = async (req, res) => {
+  const { email } = req.body;
+
+  const emailAlreadyExists = User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
