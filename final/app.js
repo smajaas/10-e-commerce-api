9d66e22483ce479be67d0b628ee155009bbb66da
smajaas <smajaas@gmail.com>
2024-03-11 10:33:17 +0530

    auth routes structure

--- final/app.js
+++ final/app.js
@@ -12,6 +12,10 @@ const morgan = require('morgan');
 //database
 const connectDB = require('./db/connect');
 
+//routers
+
+const authRouter = require('./routes/authRoutes');
+
 //middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -22,6 +26,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('e-commerce API');
 });
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
