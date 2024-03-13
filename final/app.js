7c4b2385b12477aa4c3cabb5fb528461f1eeffc2
smajaas <smajaas@gmail.com>
2024-03-13 16:14:46 +0530

    refactor cookies setup

--- final/app.js
+++ final/app.js
@@ -8,6 +8,7 @@ const app = express();
 // rest of the packages
 
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 //database
 const connectDB = require('./db/connect');
@@ -22,8 +23,13 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+  res.send('e-commerce API');
+});
+app.get('/api/v1', (req, res) => {
+  console.log(req.cookies);
   res.send('e-commerce API');
 });
 
